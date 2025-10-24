f910372778cb1a0a6c51fd76444e6d0fa40cb3a3
Allanzin178 <allan.miron@hotmail.com>
2025-10-24 16:16:45 -0300

    Conectando ao driver do neo4j

---
+++ src/config/neo4j.config.ts
@@ -0,0 +1,72 @@
+import neo4j, { Driver, EagerResult } from 'neo4j-driver'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export class DriverService {
+    private driver: Driver
+    private exitSignals: Array<string> = ['SIGTERM', 'SIGINT', 'SIGQUIT', 'SIGHUP'];
+
+    constructor() {
+        if (!process.env.NEO4J_PASSWORD){
+            throw new Error("NEO4J_PASSWORD não está definido no .env")
+        }
+
+        this.driver = neo4j.driver(
+            'neo4j://localhost',
+            neo4j.auth.basic('neo4j', process.env.NEO4J_PASSWORD)
+        )
+
+        this.assureConnection().then(() => {
+            console.log('✅ Sucesso na conexão ao neo4j')
+            this.setupSignalHandlers()
+        })
+        
+    }
+
+    async executeQuery(query: string, parameters?: any): Promise<EagerResult> {
+        await this.assureConnection()
+        const result = await this.driver.executeQuery(
+            query,
+            parameters,
+            { database: process.env.NEO4J_DATABASE || 'neo4j' }
+        )
+        return result
+    }
+
+    async assureConnection() {
+        try {
+            await this.driver.getServerInfo()
+        } catch (err) {
+            throw new Error('Erro na conexão ao Neo4J (provavelmente instância está offline): ' + err)
+        }
+    }
+
+    private setupSignalHandlers(){
+        this.exitSignals.forEach(signal => { // Tratamento para quando algum sinal de fechamento for enviado o driver ser fechado
+            process.on(signal, async () => {
+                console.log('🚮 Exit handler')
+                await this.closeDriver()
+
+                process.exit(0) 
+            })
+        })
+
+        if (process.env.NODE_ENV === 'development'){ // Tenta tratar de quando o nodemon reinicia, e fecha o driver (nao consegui fazer um log)
+            process.on('SIGUSR2', async () => {
+                await this.closeDriver();           
+                process.kill(process.pid, 'SIGUSR2'); 
+            });
+        }
+        
+    }
+
+    private async closeDriver(){
+        try{
+            console.log('❌ Fechando driver')
+            await this.driver.close()
+        }catch(err){
+            console.log("Erro ao fechar o driver: ", err)
+        }
+    }
+}
+
