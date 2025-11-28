62daed4094f45df329bd20e53464b998570ea763
Allanzin178 <allan.miron@hotmail.com>
2025-11-28 10:59:16 -0300

    Adicionando interfaces para regras e riscos

--- src/interfaces/regra-avaliacao-risco.interface.ts
+++ src/interfaces/regra-avaliacao-risco.interface.ts
@@ -1,6 +1,7 @@
 import type { TransactionEntity } from "../entities/transaction.entity.ts";
+import type RiscoInterface from "./risco.interface.ts";
 
 export default interface RegraAvaliacaoRisco {
     peso: number;
-    avaliarRisco(transacao: TransactionEntity): number;
+    avaliarRisco(transacao: TransactionEntity): Promise<RiscoInterface>;
 }
---
+++ src/interfaces/risco.interface.ts
@@ -0,0 +1,4 @@
+export default interface RiscoInterface {
+    valor: number,
+    motivo: string,
+}
